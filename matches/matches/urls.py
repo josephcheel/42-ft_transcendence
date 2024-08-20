be80ddfb618d205221db94a89025e6d686506d83
Luis <luissoto2094@gmail.com>
2024-08-20 12:32:03 +0200

    Modified gamestats to matches

--- matches/matches/urls.py
+++ matches/matches/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for gamestats project.
+URL configuration for matches project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
