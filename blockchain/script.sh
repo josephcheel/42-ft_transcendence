fe15f607301ef1ac7bcd807dd3954f41fc465d94
eavedill <eavedillo@yahoo.es>
2024-08-27 12:31:39 +0200

    added Blockchain instalation instructions

--- blockchain/script.sh
+++ blockchain/script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo modprobe fuse
+sudo groupadd fuse
 
+user="$(whoami)"
+sudo usermod -a -G fuse $user
 # This script is used to start the blockchain network
 /tmp/ganache.AppImage --host
