1e4bb672dadcff1c2cdb5cbdac0e66e7a5f06ac5
eavedill <eavedillo@yahoo.es>
2024-08-31 11:56:32 +0200

    new folder for shared functions

--- tournements/tournementsapp/models.py
+++ tournements/tournementsapp/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Tournements(models.Model):
+	OPEN_TOURNEMENT = 0
+	CLOSED_TOURNEMENT = 1
+	FINISHED_TOURNEMENT = 2
+
+
+	STATUS_CHOICES = [
+		{OPEN_TOURNEMENT, "open"},
+		{CLOSED_TOURNEMENT, "closed"},
+		{FINISHED_TOURNEMENT, "finished"},
+	]
 	name = models.CharField(max_length=30)
-	Date = models.DateTimeField()
+	date = models.DateTimeField()
 	max_players = models.IntegerField()
 	price_1 = models.IntegerField()
 	price_2 = models.IntegerField()
@@ -11,7 +21,9 @@ class Tournements(models.Model):
 	winner = models.CharField(max_length=30)
 	second = models.CharField(max_length=30)
 	third = models.CharField(max_length=30)
-	hashprevius = models.CharField(max_length=256)
+	status = models.CharField(
+		max_length=8, choices=STATUS_CHOICES, default=OPEN_TOURNEMENT)
+	hash_previus = models.CharField(max_length=256)
 	hash = models.CharField(max_length=256)
 
 class matches(models.Model):
@@ -22,8 +34,8 @@ class matches(models.Model):
 	winner = models.CharField(max_length=30)
 	round = models.IntegerField()
 
-class players(models.Model):
+class T_players(models.Model):
 	tournement = models.ForeignKey(Tournements, on_delete=models.CASCADE)
-	name = models.CharField(max_length=30)
+	player_id = models.CharField(max_length=30)
 	price = models.IntegerField()
 	round = models.IntegerField()
--- tournements/tournementsapp/views.py
+++ tournements/tournementsapp/views.py
@@ -4,50 +4,91 @@ from .wrappers import validate_credentials, require_post, require_get
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 import json
+from .models import Tournements, matches, players
 try: 
-    from usermodel.models import User
+	from usermodel.models import User
 except:
-    pass
+	pass
 
 User = get_user_model()
 
 # Create your views here.
 def custom_404_view(request, exception=None):
-    response_data = {
-        'status': 'error',
-        'message': 'The requested resource was not found',
-        'data': None
-    }
-    return JsonResponse(response_data, status=404)
+	response_data = {
+		'status': 'error',
+		'message': 'The requested resource was not found',
+		'data': None
+	}
+	return JsonResponse(response_data, status=404)
+
+@csrf_exempt
+@validate_credentials
+def list_tournements(request):
+	if request.method == 'GET':
+		player = request.username
+		try:
+			try:
+				player = User.objects.get(username=player)
+			except User.DoesNotExist:
+				return JsonResponse({'status': 'error', 'message': 'A user does not exist', 'data': None}, status=404)
+			tournements = Tournements.objects.filter(player_id=player)
+			data = serialize('json', tournements)
+			data = json.loads(data)
+			data = [entry['fields'] for entry in data]
+			return JsonResponse({'status': 'success', 'message': 'List of tournements', 'data': data}, status=200)
+		except OperationalError:
+			return JsonResponse({'status': 'error', 'message': 'Internal error', 'data': None}, status=500)
+	else:
+		return JsonResponse({'status': 'error', 'message': 'Invalid request method GET for list of tournements', 'data': None}, status=400)
 
 @csrf_exempt
 @require_post
 @validate_credentials
-def create_tournements(request):
-    username = request.username
-    try:
-        data = json.loads(request.body) 
-    except json.JSONDecodeError:
-        return JsonResponse({'status': 'error', 
-                                'message':'Invalid Json body', 
-                                'data' : None}, 
-                                status=400)    
-    tournements = Tournements.objects.all()
-    tournements_list = []
-    for tournement in tournements:
-        tournements_list.append({
-            'name': tournement.name,
-            'Date': tournement.Date,
-            'max_players': tournement.max_players,
-            'price_1': tournement.price_1,
-            'price_2': tournement.price_2,
-            'price_3': tournement.price_3,
-            'winner': tournement.winner,
-            'second': tournement.second,
-            'third': tournement.third,
-            'hashprevius': tournement.hashprevius,
-            'hash': tournement.hash
-        })
-        return JsonResponse({'status': 'success', 'message': 'Tournements fetched successfully', 'data': tournements_list}, status=200)
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid request method', 'data': None}, status=400)
+def create_tournement(request):
+	player = request.username
+	try:
+		player = User.objects.get(username=player)
+	except User.DoesNotExist:
+		return JsonResponse({'status': 'error', 'message': 'A user does not exist', 'data': None}, status=404)
+	data = request.data
+	Tournements.objects.create(name = data.get('name'), date = data.get('Date'), max_players = data.get('max_players'), price1 = data.get('price_1'), price2 = data.get('price_2'), price3 = data.get('price_3'), status = 0)
+	data_players = data.get('players')
+	for player in data_players:
+		player_id = User.objects.player.get(player)
+		players.objects.create(name = User.objects.player.get('name'), price = player.get('price'), round = player.get('round'))
+
+	return JsonResponse({'status': 'success', 'message': 'Tournement created successfully', 'data': None}, status=200)
+	
+
+@csrf_exempt
+@require_post
+@validate_credentials
+def open_tournement(request):
+	player = request.username
+
+
+@csrf_exempt
+@require_post
+@validate_credentials
+def close_tournement(request):
+	username = request.username
+	data  = request.data
+	tournements = Tournements.objects.all()
+	tournements_list = []
+	for tournement in tournements:
+		tournements_list.append({
+			'name': tournement.name,
+			'Date': tournement.Date,
+			'max_players': tournement.max_players,
+			'price_1': tournement.price_1,
+			'price_2': tournement.price_2,
+			'price_3': tournement.price_3,
+			'winner': tournement.winner,
+			'second': tournement.second,
+			'third': tournement.third,
+			'hashprevius': tournement.hashprevius,
+			'hash': tournement.hash
+		})
+		return JsonResponse({'status': 'success', 'message': 'Tournements fetched successfully', 'data': tournements_list}, status=200)
+	else:
+		return JsonResponse({'status': 'error', 'message': 'Invalid request method', 'data': None}, status=400)
