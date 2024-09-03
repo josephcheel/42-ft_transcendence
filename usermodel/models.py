a2391ff063cac99357bcb9244e978fbdbd99d6d6
Luis <luissoto2094@gmail.com>
2024-09-03 16:20:51 +0200

    copy changes of user model

--- usermodel/models.py
+++ usermodel/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 #
 class User(AbstractUser):
+    original_username =  models.CharField(max_length=100)
     def save_password(self, password):
         self.set_password(password)
         self.save()
