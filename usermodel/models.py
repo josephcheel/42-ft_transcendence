5fc6a7e834493ce458065e15b5c6e04824cab43b
Luis <luissoto2094@gmail.com>
2024-09-05 16:13:37 +0200

    updates usermodel. Includes tournament_name field and update_fields method

--- usermodel/models.py
+++ usermodel/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-#
 class User(AbstractUser):
     original_username =  models.CharField(max_length=100)
+    tournament_name = models.CharField(max_length=100)
+    def update_fields(self, **kwargs):
+        for field in kwargs:
+            if field in ['first_name', 'last_name', 'tournament_name'] and hasattr(self, field):
+                setattr(self, field, kwargs[field])
+        self.save()
