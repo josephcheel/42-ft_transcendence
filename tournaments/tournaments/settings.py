e2e22898aa27bdd8904f3ad1d4476ce77e5527e3
ernesto.avedillo <eavedillo@yahoo.es>
2024-09-23 15:03:53 +0200

    error in name

--- tournaments/tournaments/settings.py
+++ tournaments/tournaments/settings.py
@@ -183,7 +183,7 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 # Celery settings
-ELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://127.0.0.1:6379/0")
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://127.0.0.1:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://127.0.0.1:6379/0")
 CELERY_ACCEPT_CONTENT = ['json']
 
