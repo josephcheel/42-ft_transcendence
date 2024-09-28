25784fd7b1f79858986509b208c7252d0c45e338
ernesto.avedillo <eavedillo@yahoo.es>
2024-09-28 11:06:36 +0200

    DB reorganized. usermodel remeved and maintained usermanagament model. Doker compose and migrator reworked to match this new structure

--- usermanagement/user/models.py
+++ usermanagement/user/models.py
@@ -2,23 +2,23 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.conf import settings
 
-if settings.DEBUG:
-    from django.contrib.auth.models import AbstractUser
-    class User(AbstractUser):
-        original_username =  models.CharField(max_length=100, null=True)
-        tournament_name = models.CharField(max_length=100, null = True)
-        puntos = models.IntegerField(default=1000)
-        puntos_reservados = models.IntegerField(default=0)
-        # Specify a unique related_name for the groups field
-        groups = models.ManyToManyField(
-                'auth.Group', related_name='users_db_Group', blank=True)
-        user_permissions = models.ManyToManyField(
-                'auth.Permission', related_name='users_db_Permission', blank=True)
-        def update_fields(self, **kwargs):
-            for field in kwargs:
-                if field in ['first_name', 'last_name', 'tournament_name'] and hasattr(self, field):
-                    setattr(self, field, kwargs[field])
-            self.save()
+#if settings.DEBUG:
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    original_username =  models.CharField(max_length=100, null=True)
+    tournament_name = models.CharField(max_length=100, null = True)
+    puntos = models.IntegerField(default=1000)
+    puntos_reservados = models.IntegerField(default=0)
+    # Specify a unique related_name for the groups field
+    groups = models.ManyToManyField(
+            'auth.Group', related_name='users_db_Group', blank=True)
+    user_permissions = models.ManyToManyField(
+            'auth.Permission', related_name='users_db_Permission', blank=True)
+    def update_fields(self, **kwargs):
+        for field in kwargs:
+            if field in ['first_name', 'last_name', 'tournament_name'] and hasattr(self, field):
+                setattr(self, field, kwargs[field])
+        self.save()
 
 User = get_user_model()
 
