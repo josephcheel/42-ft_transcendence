5239c6d8a6233de9acb6831a11d705e8889d92fe
eavedill <eavedillo@yahoo.es>
2024-10-05 14:42:57 +0200

    add urls for blockchain

--- tournaments/blockchainapp/urls.py
+++ tournaments/blockchainapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from blockchainapp.views import connection
+from blockchainapp.views import execute_contract, get_balance, make_transaction
 
 urlpatterns = [
-	path('connect/', connection),
+	path('execute_contract/', execute_contract),
+	path('get_balance/', get_balance),
+	path('make_transaction/', make_transaction),
 ]
