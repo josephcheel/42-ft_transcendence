5433b0e897458b9e6f5b2efb2f597d83870ea40b
eavedill <eavedillo@yahoo.es>
2024-10-06 18:23:31 +0200

    adding tests for usermanagement to gateway

--- gateway/test.sh
+++ gateway/test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "Running tests"
 echo "Create user"
-curl -X POST  http://usermanagement:8000/user/create_user     -H "Content-Type: application/json"      -d '{"username": "test", "password": "test","first_name":"aaa", "last_name":"bbb"}' -d '{"username": "test", "password": "test","first_name":"aaa", "last_name":"}'
+curl -c cookies1.txt -X POST  http://usermanagement:8000/user/create_user     -H "Content-Type: application/json"      -d '{"username": "test1", "password": "test","first_name":"aaa", "last_name":"bbb"}' -d '{"username": "test1", "password": "test1","first_name":"aaa", "last_name":"}'
 
 echo "Login"
 
-curl -X POST  http://usermanagement:8000/user/login_user     -H "Content-Type: application/json"      -d '{"username": "test", "password": "test"}'
+curl -o cookies1.txt -X POST  http://usermanagement:8000/user/login_user     -H "Content-Type: application/json"      -d '{"username": "test1", "password": "test"}'
 
 echo "list users"
 
-curl -X GET  http://usermanagement:8000/user/list_users     -H "Content-Type: application/json"      -d '{"username": "test", "password": "test"}'
+curl -o cookies1.txt -X GET  http://usermanagement:8000/user/list_users     -H "Content-Type: application/json"      -d '{"username": "test", "password": "test"}'
