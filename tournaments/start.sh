7eab8964af097d45324c5301579f50e5e8e91a4c
eavedill <eavedillo@yahoo.es>
2024-10-06 18:20:44 +0200

    wait untid DB has started

--- tournaments/start.sh
+++ tournaments/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-python manage.py makemigrations tournamentsapp
-python manage.py migrate
+
+chmod +x ./wait_for_it.sh
+./wait_for_it.sh ${DB_HOST}:${DB_PORT} --timeout=0
+
+python manage.py makemigrations
+python manage.py migrate tournamentsapp
 python manage.py runserver 0.0.0.0:8000
  
