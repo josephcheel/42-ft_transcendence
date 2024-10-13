495e37015a19386aad04b8edfff5539d9a573ff8
eavedill <eavedillo@yahoo.es>
2024-10-13 13:55:15 +0200

    remove wait for it

--- usermanagement/start.sh
+++ usermanagement/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-chmod +x ./wait_for_it.sh
-./wait_for_it.sh ${DB_HOST}:${DB_PORT} --timeout=0
-
 #python manage.py makemigrations
 #python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
