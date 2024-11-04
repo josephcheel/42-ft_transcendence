e9a9c2907c5420380c4981027bdeb4e23c3a809c
eavedill <eavedillo@yahoo.es>
2024-11-04 19:09:43 +0100

    Correction iin list of matches, player could be as player 1 or player 2.

--- tournaments/tournamentsapp/views/list_matches.py
+++ tournaments/tournamentsapp/views/list_matches.py
@@ -3,6 +3,7 @@ from tournamentsapp.models import Matches
 from datetime import datetime
 from django.db import OperationalError
 from django.http import JsonResponse
+from django.db.models import Q
 import json
 
 from user.models import User
@@ -12,12 +13,8 @@ from user.models import User
 def list_matches(request):
 	player = request.user.username
 	try:
-#		try:
-#			player = User.objects.get(username=player)
-#		except User.DoesNotExist:
-#			return JsonResponse({'status': 'error', 'message': 'A user does not exist', 'data': None}, status=404)
 		player = User.objects.get(username=request.username)
-		matches_data = Matches.objects.filter(player_id=player.id)
+		matches_data = Matches.objects.filter(Q(player_id1=player.id) | Q(player_id2=player.id))
 		matches_list = list(matches_data.values())
 		for match in matches_list:
 			for key, value in match.items():
