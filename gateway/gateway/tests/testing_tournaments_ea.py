9d83a15e38fe805b0e794ce21f1e1d3cad1746df
eavedill <eavedillo@yahoo.es>
2024-11-10 20:45:55 +0100

    Admint None in fields tournament_id and round in Matches to be able to add Matches without tournaments

--- gateway/gateway/tests/testing_tournaments_ea.py
+++ gateway/gateway/tests/testing_tournaments_ea.py
@@ -2,138 +2,140 @@ import requests
 from datetime import datetime, timedelta
 import urllib3
 import json
+import random
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-DOMAIN = '10.11.250.2'
-#DOMAIN = 'localhost'
+#DOMAIN = '10.11.250.2'
+DOMAIN = '192.168.40.44'
 
 
 def get_csrf_token(session, url):
-    # Step 1: Make a GET request to obtain CSRF cookie
-    response = session.get(url)
-    # The CSRF token is usually stored in the cookies
-    csrf_token = response.cookies.get('csrftoken')
-    return csrf_token
+	# Step 1: Make a GET request to obtain CSRF cookie
+	response = session.get(url)
+	# The CSRF token is usually stored in the cookies
+	csrf_token = response.cookies.get('csrftoken')
+	return csrf_token
 
 content_json_type = 'application/json'
 referer = f'https://{DOMAIN}:8000'
 csrf_url = f'https://{DOMAIN}:8000/api/get_cookie/'
 register_url = f'https://{DOMAIN}:8000/api/user/create_user/'
 login_url = f'https://{DOMAIN}:8000/api/user/login_user/'
-get_profile_url = f'https://{DOMAIN}:8000/api/user/get_profile/1'
+get_profile_url = f'https://{DOMAIN}:8000/api/user/get_profile'
 create_tournament_url = f'https://{DOMAIN}:8000/api/tournaments/open_tournament/'
 accept_invitation = f'https://{DOMAIN}:8000/api/tournaments/accept_invitation/'
 close_tournament = f'https://{DOMAIN}:8000/api/tournaments/close/'
 finish_match = f'https://{DOMAIN}:8000/api/tournaments/finish_match/'
 list_matches = f'https://{DOMAIN}:8000/api/tournaments/list_matches/'
 list_matches_username = f'https://{DOMAIN}:8000/api/tournaments/list_matches/1'
-
+total_players = 8
 
 
 
 
 def get_request(session, url, csrf_token, data=None):
-    # Add CSRF token to the headers
-    headers = {
-        'Referer': referer,
-        'X-CSRFToken': csrf_token,
-        'Content-Type': content_json_type
-    }
+	# Add CSRF token to the headers
+	headers = {
+		'Referer': referer,
+		'X-CSRFToken': csrf_token,
+		'Content-Type': content_json_type
+	}
 
-    # Make the POST request to log in
-    response = session.get(url, headers=headers)
-    return response
+	# Make the POST request to log in
+	response = session.get(url, headers=headers)
+	return response
 
 
 def send_request(session, url, csrf_token, data=None):
-    # Add CSRF token to the headers
-    headers = {
-        'Referer': referer,
-        'X-CSRFToken': csrf_token,
-        'Content-Type': content_json_type
-    }
+	# Add CSRF token to the headers
+	headers = {
+		'Referer': referer,
+		'X-CSRFToken': csrf_token,
+		'Content-Type': content_json_type
+	}
 
-    # Make the POST request to log in
-    response = session.post(url, json=data, headers=headers)
-    return response
+	# Make the POST request to log in
+	response = session.post(url, json=data, headers=headers)
+	return response
 
 
 mysessions = {}
 csrf = {}
 
-#Test and create 21 users
+#Test and create total_players users
 def test_register_user():
-    for i in range(1, 22):
-        mysessions[i] = requests.Session()
-        mysessions[i].verify = False
-        csrf[i] = get_csrf_token(mysessions[i], csrf_url)
-        my_data = json.dumps({'username': f"test{i}", 'password': "test", 'first_name': f"test{i}",  'last_name': f"Apellido{i}"})
-        response = send_request(mysessions[i], register_url, csrf[i], my_data)
-        print(response.json())
-        print(csrf[i])
-        assert response.status_code == 201
-        assert response.json()['status'] == 'success'
-        assert response.json()['message'] == 'User created successfully'
-        my_data = json.dumps({'username': f"test{i}", 'password': "test"})
-        response = send_request(mysessions[i], login_url, csrf[i], my_data)
-        assert response.status_code == 200
-        assert response.json()['status'] == 'success'
-        assert response.json()['message'] == 'User logged in successfully'
-        response = send_request(mysessions[i], get_profile_url, csrf[i])
-        assert response.status_code == 200
-        assert response.json()['status'] == 'success'
-        assert response.json()['message'] == 'User profile retrieved successfully'
-        assert response.json()['data']['username'] == f'test{i}'
+	for i in range(1, total_players + 1):
+		mysessions[i] = requests.Session()
+		mysessions[i].verify = False
+		csrf[i] = get_csrf_token(mysessions[i], csrf_url)
+		my_data = {'username': f"test{i}", 'password': "test", 'first_name': f"test{i}",  'last_name': f"Apellido{i}"}
+		response = send_request(mysessions[i], register_url, csrf[i], my_data)
+		print(response.status_code, response.json())
+		if response.status_code == 201:
+			assert response.json()['status'] == 'success'
+			assert response.json()['message'] == 'User created successfully'
+		else:
+			assert response.status_code == 409
+			assert response.json()['status'] == 'error'
+			assert response.json()['message'] == 'User already Exists'
+		my_data = {'username': f"test{i}", 'password': "test"}
+		response = send_request(mysessions[i], login_url, csrf[i], my_data)
+		print(response.status_code, response.json())
+		assert response.status_code == 200
+		assert response.json()['status'] == 'success'
+		assert response.json()['message'] == 'user is logged in'
+		response = send_request(
+			mysessions[i], get_profile_url + '/' + f"test{i}", csrf[i])
+		print(response.status_code, response.json())
+		assert response.status_code == 200
+		assert response.json()['status'] == 'success'
+		assert response.json()['message'] == 'Got user profile'
+		assert response.json()['data']['username'] == f'test{i}'
+
+def play_match():
+	print("log in all players")
+	for i in range(1, total_players + 1):
+		my_data = {'username': f"test{i}", 'password': "test"}
+		response = send_request(mysessions[i], login_url, csrf[i], my_data)
+	for i in range(1, 100):
+		user_nr1 = random.randint(1, total_players)
+		player1 = f"test{user_nr1}"
+		player2 = f"test{random.randint(1, total_players)}"
+		while player1 == player2:
+			player2 = f"test{random.randint(1, total_players)}"
+		winning_points = 5
+		looser_points = random.randint(0, winning_points-1)
+		if random.choice([True, False]):
+			my_data = {
+				'match_id': -1,
+				'player1': player1,
+				'player2': player2,
+				'winner': player1,
+				'looser': player2,
+				'points_winner': winning_points,
+				'points_looser': looser_points,
+			}
+		else:
+			my_data = {
+				'match_id': -1,
+				'player1': player1,
+				'player2': player2,
+				'winner': player2,
+				'looser': player1,
+				'points_winner': winning_points,
+				'points_looser': looser_points,
+			}
+		response = send_request(mysessions[user_nr1], finish_match, csrf[user_nr1], data = my_data)
+		print(response.status_code, response.json())
+		assert response.status_code == 200
+		assert response.json()['status'] == 'success'
+		assert response.json()['message'] == 'Match finished successfully'
 
 def close_sessions():
-    for i in range(1, 22):
-        mysessions[i].close()
+	for i in range(1, total_players + 1):
+		mysessions[i].close()
 # Main execution
 if __name__ == "__main__":
-    test_register_user()
-    close_sessions()
-    """response1 = send_request(session, login_url, csrf1, {
-        'username': "test",
-        'password': "test"
-    })
-     response2 = send_request(session2, login_url, csrf2,{
-        'username': "test1",
-        'password': "test"
-    })
-    current_datetime = datetime.now()
+	test_register_user()
+	play_match()
+	close_sessions()
 
-    date_start = current_datetime + timedelta(days=1)
-
-    date_start_iso = date_start.isoformat()
-
-    response1 = send_request(session, create_tournament_url, csrf1,     {
-		'date_start': date_start_iso,
-        'max_players': 16,
-        'cost': 10,
-        'price_1': 100,
-        'price_2': 50,
-        'price_3': 25,
-        'players': ['test','test1']
-    })
-    response1 = send_request(session, accept_invitation, csrf1, {
-        'tournament_id': 2
-    })
-    response1 = send_request(session2, accept_invitation, csrf2, {
-        'tournament_id': 2
-    })
-    response1 = send_request(session, close_tournament, csrf1, {
-        'tournament_id': 2
-    })
-
-    response1 = send_request(session, finish_match, csrf1, {
-        'match_id': '2',
-        'winner': 'test1',
-        'looser': 'test'
-    }) 
-
-    response1 = get_request(session, list_matches_username, csrf1)
-    
-    
-    print(response1.json())
-    session2.close()
-    session.close()
-"""
--- tournaments/tournamentsapp/views/finish_match.py
+++ tournaments/tournamentsapp/views/finish_match.py
@@ -15,8 +15,16 @@ def finish_match(request):
 	if match_id < 0:
 		player1 = User.objects.get(username=data.get('winner'))
 		player2 = User.objects.get(username=data.get('looser'))
-		Matches.objects.create(tounament_id=None, number_round=0, date_time=timezone.now(
-		), player_id_1=player1, player_id_2=player2, round=None, status=StatusMatches.PLAYED.value, winner_id = player1, points_winner=data.get("points_winner"), looser_id = player2, points_looser=data.get("points_looser"),
+		Matches.objects.create(
+			number_round=0, 
+			date_time=timezone.now(), 
+			player_id_1=player1, 
+			player_id_2=player2, 
+			round=None, 
+			status=StatusMatches.PLAYED.value, 
+			winner_id = player1, 
+			points_winner=data.get("points_winner"), looser_id = player2, 
+			points_looser=data.get("points_looser"),
 		)
 		return JsonResponse({'status': 'success', 'message': 'Match finished successfully', 'data': None}, status=200)
 	try:
