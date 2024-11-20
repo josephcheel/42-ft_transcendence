04eadced40e1f9b9e0bab4a0a3b0114c7f3112d3
josephcheel <josephcheelnakashima@gmail.com>
2024-11-20 20:47:12 +0100

    Added Images and changed GameSelection style to adjust better readability

--- frontend/src/components/GameSelection.vue
+++ frontend/src/components/GameSelection.vue
@@ -4,8 +4,8 @@
       <!-- Left Column (Vertical Grid) -->
       <div class="col-lg-3 d-flex flex-column align-items-center justify-content-start">
         <div @click="startOnlineMatch" id="simple-match" class="bento-box flex-grow-1">
-          <h2>{{ $t('games.simple_match')}}</h2>
-          <p>{{ $t('games.explain_simple_match')}}</p>
+          <h2 class="titles">{{ $t('games.simple_match')}}</h2>
+          <p class="titles">{{ $t('games.explain_simple_match')}}</p>
        </div>
         <!-- <div class="bento-box flex-grow-1">Left Grid 2</div> -->
       </div>
@@ -13,16 +13,16 @@
       <!-- Middle Column (Two Grids) -->
       <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
         <div @click="startLocalMatch" id="local-match" class="bento-box flex-grow-1">
+          <h2 class="titles">{{ $t('games.local_match')}}</h2>
+          <p class="titles">{{ $t('games.explain_local_match')}}</p>
          
-          <h2>{{ $t('games.local_match')}}</h2>
-          <p>{{ $t('games.explain_local_match')}}</p>
         </div>
         <div id="friend-match" class="bento-box flex-grow-1" @click="showFriendMatchInput">
-          <h2>{{ $t('games.friend_match')}}</h2>
-            <p>{{ $t('games.explain_friend_match')}}</p>
+          <h2 class="titles">{{ $t('games.friend_match')}}</h2>
+            <p class="titles">{{ $t('games.explain_friend_match')}}</p>
             <!-- Input para enviar invitación -->
             <div v-if="showFriendInput" class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
-              <input type="text" v-model="friendName" placeholder="Enter Match Id" class="form-control d-inline-block w-50" />
+                <input type="text" v-model="friendName" placeholder="Enter Match Id" class="form-control w-75 text-center" @keyup.enter="sendInvitation" />
               <button @click="sendInvitation" class="btn btn-success mt-2">{{ $t('games.join')}}</button>
             </div>
         </div>
@@ -32,8 +32,8 @@
       <div class="col-lg-3 d-flex flex-column align-items-center justify-content-start">
         <!-- <div class="bento-box flex-grow-1">Right Grid 1</div> -->
         <div @click="goToTournament" id="tournament" class="bento-box flex-grow-1">
-        <h2>{{ $t('games.tournament')}}</h2>
-        <p>{{ $t('games.explain_tournament')}}</p>
+        <h2 class="titles">{{ $t('games.tournament')}}</h2>
+        <p class="titles">{{ $t('games.explain_tournament')}}</p>
       </div>
       </div>
     </div>
@@ -59,7 +59,6 @@ export default {
       this.$router.push({name: 'Game'});
     },
     startOnlineMatch() {
-      alert('Starting a online match...');
       this.$router.push({name: 'GameOnline'});
     },
     showFriendMatchInput() {
@@ -84,28 +83,19 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  font-size: 2.5em !important;
+}
 .bento-grid-container {
   height: 100%; /* Full height container */
   width: 60vh; /* Full width */
   display: flex;
   justify-content: center;
 }
-
-.bento-box2 {
-  width:  100%; /* Ensure boxes take full width of their column */
-  margin: 0.5em;
-  border-radius: 15px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s ease;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #f5f5f5; /* Light background for visibility */
-  font-family: 'Nokia Cellphone FC' ;
-  color: whitesmoke;
-  filter:brightness(0.9);
-}
+.form-control:focus {
+    outline: none;
+    box-shadow: none;
+  }
 
 .bento-box {
   width: 100%; /* Ensure boxes take full width of their column */
@@ -130,14 +120,11 @@ export default {
   transform: scale(1.05);
   filter:brightness(1);
   filter:drop-shadow(0 0 0.75rem #f5f5f5);
-  /* filter:drop-shadow(0 0 0.75rem #37f2ff); */
-  /* filter:contrast(1) */
-  /* filter:grayscale(0%) */
 }
-
+/* 
 .flex-grow-1 {
-  flex-grow: 1; /* Allow boxes to grow and fill space vertically */
-}
+  flex-grow: 1; 
+} */
 
 #simple-match {
   background-image: url('/assets/images/online-match.png');
@@ -147,7 +134,7 @@ export default {
 }
 
 #local-match {
-  background-image: url('/assets/images/local-match.png');
+  background-image: url('/assets/images/local-match2.png');
   background-size: cover;
   background-position: center;
   /* filter:grayscale(50%) */
@@ -155,55 +142,33 @@ export default {
 
 #friend-match {
   background-color: #ffc107; /* Yellow */
-}
-
-#tournament {
-  /* background-color: #dc3545; Red */
-  background-image: url('/assets/images/tournament.png');
+  background-image: url('/assets/images/friend-match6.png');
   background-size: cover;
   background-position: center;
 }
 
-</style>
-
-<!-- <script setup>
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import { isAuthorized } from '../utils/isAuthorized';
-
-const username = ref('');
-const router = useRouter();
-
-const showFriendInput = ref(false);
-const friendName = ref('');
-
-function startLocalMatch() {
-  alert('Starting a local match...');
-  router.push({name: 'Game'});
+#tournament {
+  background-image: url('/assets/images/tournament3.png');
+  background-size: cover;
+  background-position: center;
 }
 
-function startOnlineMatch() {
-  alert('Starting a online match...');
-  router.push({name: 'GameOnline'});
-}
-
-function showFriendMatchInput() {
-  showFriendInput.value = true;
-}
-
-function goToMatch() {
-  if (friendName.value) {
-    // Programmatically navigate to '/route1/:id'
-    router.push(`/game-online?match-id=${friendName.value}`);
+@media screen and (max-width: 768px) {
+  #tournament {
+    background-image: url('/assets/images/tournament2.PNG');
   }
+  #friend-match {
+    background-image: url('/assets/images/friend-match7.png');
+  }
+  /* #local-match{
+    display: none;
+  } */
 }
 
-function sendInvitation() {
-  alert(`Invitation sent to ${friendName.value}`);
-  goToMatch();
+.titles {
+  color: whitesmoke;
+  filter:brightness(1);
+  filter:drop-shadow(0 0 0.75rem #ffffffbd);
 }
 
-function goToTournament() {
-  alert('Navigating to tournament creation...');
-}
-</script> -->
+</style>
