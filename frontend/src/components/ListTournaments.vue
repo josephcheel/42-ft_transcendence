b6b245e9fb3b905e35906b865f29f37b5788224a
Eloi Alonso Quiñonero <97552454+Develoi89@users.noreply.github.com>
2024-12-04 21:24:44 +0100

    listtournament component done

--- frontend/src/components/ListTournaments.vue
+++ frontend/src/components/ListTournaments.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="tournament-list">
-    <h3>Lista de Torneos</h3>
+    <h3>{{ $t('listtournaments.list_tournaments')}}</h3>
     <div v-if="loading" class="text-center">
-      <span>Cargando torneos...</span>
+      <span>{{ $t('listtournaments.loading_tournaments')}}</span>
     </div>
     <div v-if="error" class="text-danger text-center">
       <p>{{ error }}</p>
@@ -10,10 +10,10 @@
     <ul v-if="tournaments.length" class="list-group">
       <li v-for="tournament in tournaments" :key="tournament.id" class="list-group-item">
         <h5>{{ tournament.name }}</h5>
-        <p><strong>Fecha:</strong> {{ tournament.date || "No especificada" }}</p>
+        <p><strong>{{ $t('tournaments.tournament_date')}}</strong> {{ tournament.date_start || "No especificada" }}</p>
       </li>
     </ul>
-    <p v-else class="text-center">No hay torneos disponibles.</p>
+    <p v-else class="text-center">{{ $t('listtournaments.empty_tournaments_list')}}</p>
   </div>
 </template>
 
@@ -29,12 +29,6 @@ export default {
       error: null,
     };
   },
-  mounted() {
-    const username = localStorage.getItem('username');
-    if (!username) {
-      this.$router.push({ path: '/', params: { currentView: 'Login' } });
-    }
-  },
   props: {
     username: {
       type: String,
@@ -42,23 +36,28 @@ export default {
       default: ''
     },
   },
+  mounted() {
+    const username = localStorage.getItem('username');
+    if (!username) {
+      this.$router.push({ path: '/', params: { currentView: 'Login' } });
+    }
+  },
   methods: {
     async fetchTournaments() {
       this.loading = true;
       this.error = null;
-      console.log(this.username);
+      const username = localStorage.getItem('username');
       try {
-        const response = await axios.get(
-          `https://${this.$router.ORIGIN_IP}:8000/api/tournaments/list_tournaments/${this.username}`
-        );
+        const response = await axios.get(`https://${this.$router.ORIGIN_IP}:8000/api/tournaments/list_tournaments/${username}/`);
         if (response.data.status === "success") {
           this.tournaments = JSON.parse(response.data.data);
+          console.log(response);
         } else {
-          this.error = response.data.message || "Error desconocido al cargar los torneos.";
+          this.error = response.data.message || "Unknowed error at load tournaments.";
         }
       } catch (err) {
-        this.error = "No se pudo conectar con el servidor.";
-        console.error("Error al cargar los torneos:", err);
+        this.error = "Error conection server.";
+        console.error("Error at load tournaments:", err);
       } finally {
         this.loading = false;
       }
