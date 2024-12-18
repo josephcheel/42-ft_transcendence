40323515ebda69fc5f69b6b91b74a319738e4034
Luis <luissoto2094@gmail.com>
2024-12-18 14:18:14 +0100

    saves tournaments matches correctly

--- tournaments/tournamentsapp/views/finish_match.py
+++ tournaments/tournamentsapp/views/finish_match.py
@@ -15,8 +15,7 @@ logger = logging.getLogger('django')
 def finish_match(request):
 	data = request.data
 	match_id = request.data.get('match_id')
-
-	if match_id < 0:
+	if type(match_id) == int and match_id < 0:
 		player1 = User.objects.get(username=data.get('winner'))
 		player2 = User.objects.get(username=data.get('looser'))
 		Matches.objects.create(
@@ -33,7 +32,7 @@ def finish_match(request):
 		)
 		return JsonResponse({'status': 'success', 'message': 'Match finished successfully', 'data': None}, status=200)
 	try:
-		match = Matches.objects.get(id=match_id)
+		match = Matches.objects.get(match_UUID=match_id)
 	except Matches.DoesNotExist:
 			return JsonResponse({'status': 'error', 'message': 'The match does not exist', 'data': None}, status=400)
 
