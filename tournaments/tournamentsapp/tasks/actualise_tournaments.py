c52326e13f75d87842de066ad31a19da088ba72f
ernesto.avedillo <eavedillo@yahoo.es>
2024-12-19 12:52:56 +0100

    if one semifinal is aborted repeat the other semifinal in the final

--- tournaments/tournamentsapp/tasks/actualise_tournaments.py
+++ tournaments/tournamentsapp/tasks/actualise_tournaments.py
@@ -84,11 +84,18 @@ def actualise_tournament(tournament_id):
 			# There can 0, 1 or 2 matches. 0 if both are aborted 1 or 2 if they are OKAY
 			# We need to create the final round if it's not equal to 0
 			if mymatches.count() != 0:
-	
+				if matches.count() == 2:
+					player1= mymatches[0].winner_id
+					player2 = mymatches[1].winner_id
+				else:
+					player1 = mymatches[0].winner_id
+					player2= mymatches[0].looser_id
 				match_final = Matches.objects.create(
 					tournament_id=mymatches[0].tournament_id,
 					player_id_1=mymatches[0].winner_id,
-					player_id_2= None if mymatches.count() == 1 else  mymatches[1].winner_id, 
+					player_id_2= mymatches[0].looser_id if mymatches.count() == 1 else  mymatches[1].looser_id, 
+					winner_id=mymatches[0].winner_id,
+					looser_id=mymatches[0].looser_id,
 					date_time=tournament.last_match_date + timedelta(minutes=TIME_DELTA * 2), 
 					round=Rounds.FINAL_ROUND.value, 
 					number_round=1,
